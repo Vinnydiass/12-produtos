1e2e4c1f243b956a2fdebcee866ad80a1a48ef32
unknown <23114290054@ACADEMICO.IESB.BR>
2024-05-15 20:24:39 -0300

    first commit

---
+++ src/routes/routes.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const router = express.Router()
+
+const ProdutoController = require('../controllers/Produto.Controller')
+
+router.get('/', (req, res) =>{
+    res.json("Ok")
+})
+
+router.get('/produtos', ProdutoController.getAll)
+router.post('/produtos', ProdutoController.create)
+router.get('/produtos/:id', ProdutoController.getById)
+router.put('/produtos/:id', ProdutoController.update)
+router.delete('/produtos/:id', ProdutoController.deleteProduct)
+
+module.exports = router
